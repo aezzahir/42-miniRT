93847d5d14b1dd3ffab961eb2784a9e3fe82ef55
Ilyes Ben Haj Dahmane <iben-haj@c4r8p8.1337.ma>
2024-11-23 11:36:47 +0100

    norminette in progress

--- src/utils/rays.c
+++ src/utils/rays.c
@@ -6,7 +6,7 @@
 /*   By: iben-haj <iben-haj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:02:31 by iben-haj          #+#    #+#             */
-/*   Updated: 2024/11/22 17:02:32 by iben-haj         ###   ########.fr       */
+/*   Updated: 2024/11/23 11:24:53 by iben-haj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,52 @@ static t_color	handle_no_intersection(t_scene *scene)
 	return (color_scale(scene->ambient.color, scene->ambient.ratio));
 }
 
-static t_color	compute_lighting(t_scene *scene, t_intersection *intersection,
-		t_ray *ray)
+// Helper function to calculate lighting vectors
+static t_lighting_vectors calculate_vectors(t_scene *scene, 
+    t_intersection *intersection, t_ray *ray)
 {
-	t_vector	normal;
-	t_vector	light_dir;
-	t_vector	view_dir;
-	t_color		ambient;
-	t_color		diffuse;
-	t_color		specular;
-	float		shadow_factor;
-	t_color		local_color;
+    t_lighting_vectors vectors;
+    
+    vectors.normal = ft_get_surface_normal_vector(intersection);
+    vectors.light_dir = vector_normalize(vector_subtract(scene->light.position,
+        intersection->point));
+    vectors.view_dir = vector_normalize(vector_negate(ray->direction));
+    
+    return (vectors);
+}
 
-	normal = ft_get_surface_normal_vector(intersection);
-	light_dir = vector_normalize(vector_subtract(scene->light.position,
-				intersection->point));
-	view_dir = vector_normalize(vector_negate(ray->direction));
-	ambient = calculate_ambient(scene, intersection->color);
-	diffuse = calculate_diffuse(scene, normal, light_dir, intersection->color);
-	specular = calculate_specular(scene, normal, light_dir, view_dir);
-	shadow_factor = calculate_shadow(scene, intersection->point, light_dir);
-	local_color = color_add(ambient, color_scale(color_add(diffuse, specular),
-				shadow_factor));
-	return (local_color);
+// Helper function to calculate colors
+static t_color calculate_colors(t_scene *scene, t_intersection *intersection,
+    t_lighting_vectors vectors)
+{
+    t_color ambient;
+    t_color diffuse;
+    t_color specular;
+    float shadow_factor;
+    
+    ambient = calculate_ambient(scene, intersection->color);
+    diffuse = calculate_diffuse(scene, vectors.normal, vectors.light_dir,
+        intersection->color);
+    specular = calculate_specular(scene, vectors.normal, vectors.light_dir,
+        vectors.view_dir);
+    shadow_factor = calculate_shadow(scene, intersection->point,
+        vectors.light_dir);
+    
+    return (color_add(ambient, color_scale(color_add(diffuse, specular),
+        shadow_factor)));
+}
+
+// Main lighting computation function
+static t_color compute_lighting(t_scene *scene, t_intersection *intersection,
+    t_ray *ray)
+{
+    t_lighting_vectors vectors;
+    t_color local_color;
+    
+    vectors = calculate_vectors(scene, intersection, ray);
+    local_color = calculate_colors(scene, intersection, vectors);
+    
+    return (local_color);
 }
 
 t_color	trace_ray(t_ray *ray, t_scene *scene, int depth)
